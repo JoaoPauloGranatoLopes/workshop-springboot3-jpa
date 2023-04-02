5d1cd57143061f8344212c417474e726a77938c5
João Paulo Granato Lopes <jpglopes@yahoo.com.br>
2023-04-02 19:43:25 -0300

    H2 database, test profile, JPA - review

--- src/main/java/com/application/JavaCompletoSpringBootApplication.java
+++ src/main/java/com/application/JavaCompletoSpringBootApplication.java
@@ -2,7 +2,9 @@ package com.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = "com.entities")
 @SpringBootApplication
 public class JavaCompletoSpringBootApplication {
 
