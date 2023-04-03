6c1022f9d81a09d649396145b153d0d7d97974bd
João Paulo Granato Lopes <jpglopes@yahoo.com.br>
2023-04-03 13:05:24 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/application/JavaCompletoSpringBootApplication.java
+++ src/main/java/com/application/JavaCompletoSpringBootApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan(basePackages = "com.entities")
+@EntityScan(basePackages = ("com.entities"))
 @SpringBootApplication
 public class JavaCompletoSpringBootApplication {
 
